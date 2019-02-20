793d03185d0fb2cb0e1a5266bec532f7f6a7ff43
Christopher Harrison <cnharrison@gmail.com>
2019-02-20 17:36:12 -0500

    remove pdfs i made :X

--- js/index.js
+++ js/index.js
@@ -7,9 +7,7 @@ class TabLink {
     this.itemElement = document.querySelector(
       `.tabs-item[data-tab="${this.tabData}"]`
     );
-
     this.tabLink = new TabItem(this.itemElement);
-
     this.element.addEventListener("click", () => {
       this.select();
     });
@@ -17,13 +15,10 @@ class TabLink {
 
   select() {
     const links = document.querySelectorAll(".tabs-link");
-
     links.forEach(link => {
       link.classList.remove("tabs-link-selected");
     });
-
     this.element.classList.add("tabs-link-selected");
-
     this.tabLink.select();
   }
 }
@@ -32,14 +27,11 @@ class TabItem {
   constructor(element) {
     this.element = element;
   }
-
   select() {
     const items = document.querySelectorAll(".tabs-item");
-
     items.forEach(item => {
       item.classList.remove("tabs-item-selected");
     });
-
     this.element.classList.add("tabs-item-selected");
   }
 }
