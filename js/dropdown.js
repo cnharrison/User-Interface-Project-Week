6308669086adc147ab209d10f51f34bda79592ca
Christopher Harrison <cnharrison@gmail.com>
2019-02-21 21:20:09 -0500

    navigation system desktop view

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,25 +1,24 @@
 class Dropdown {
-    constructor(element) {
-      // Assign this.element to the dropdown element
-      this.element = element;
-      // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-      this.button = this.element.querySelector(".dropdown-button");
-  
-  
-      // assign the reference to the ".dropdown-content" class found in the dropdown element
-      this.content = this.element.querySelector(".dropdown-content");
-  
-      // Add a click handler to the button reference and call the toggleContent method.
-      this.button.addEventListener("click", this.toggleContent.bind(this));
-    }
-  
-    toggleContent() {
-      // Toggle the ".dropdown-hidden" class off and on
-      this.content.classList.toggle("dropdown-hidden");
-      console.log("tg fired bro");
+  constructor(element) {
+    this.element = element;
+    this.button = this.element.querySelector(".dropdown-button");
+    this.content = this.element.querySelector(".dropdown-content");
+    this.stuff = this.element.querySelector(".dropdown-stuff");
+    this.button.addEventListener("click", this.toggleContent.bind(this));
+  }
+
+  toggleContent() {
+    console.log("toggle content fired dude");
+    this.content.classList.toggle("dropdown-hidden");
+    this.stuff.classList.toggle("dropdown-hidden");
+    if (this.button.src.endsWith("img/nav-hamburger.png") === true) {
+      this.button.src = "img/nav-hamburger-close.png";
+    } else {
+      this.button.src = "img/nav-hamburger.png";
     }
   }
-  // Nothing to do here, just study what the code is doing and move on to the Dropdown class
-  let dropdowns = document
-    .querySelectorAll(".dropdown")
-    .forEach(dropdown => new Dropdown(dropdown));
+}
+
+let dropdowns = document
+  .querySelectorAll(".dropdown")
+  .forEach(dropdown => new Dropdown(dropdown));
